5e3adce65dbe0e246ba37b8343c1ed5980fb9e9d
Berry  Semexan <bsemexan@gmail.com>
2018-04-19 23:05:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,8 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
-function addElementToBeginningOfArray(){
+function addElementToBeginningOfArray(arr, elem){
 
 }
 
 function destructivelyAddElementToBeginningOfArray() {
-  
+
 }
