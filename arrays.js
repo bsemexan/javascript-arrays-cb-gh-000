c2ba2f8b8dea870a73f10fe676a66bfc35522671
Berry  Semexan <bsemexan@gmail.com>
2018-04-19 23:18:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(arr, elem){
-  arr = [elem, ...arr]
+  return [elem, ...arr]
 }
 
 function destructivelyAddElementToBeginningOfArray(arr, elem) {
